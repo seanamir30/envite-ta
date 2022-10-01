64b65ee58b3414e3d3b529d36064cc1a9cba5154
Sean Cafe <seanamir30@gmail.com>
2022-10-01 16:24:52 +0800

    Functioning edit date functionality

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'white': '#ffffff',
+      'transparent': 'transparent',
+      'primary-1': '#240D57',
+      'primary-2': '#501FC1',
+      'primary-3': '#8456EC',
+      'primary-4': '#E87BF8',
+      'secondary-1': '#CCB6FF',
+      'secondary-2': '#EDE5FF',
+      'secondary-3': '#F6F2FF',
+      'secondary-4': '#FBFAFF',
+      'alert-1': '#FFD7E0',
+      'alert-2': '#E61445',
+      'alert-3': '#D3FFE2',
+      'alert-4': '#00805E',
+      'neutral-1': '#4F4F4F',
+      'neutral-2': '#828282',
+      'neutral-3': '#BDBDBD',
+      'neutral-4': '#E0E0E0',
+      'neutral-5': '#F2F2F2',
+      'neutral-6': '#BDBDBD',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
